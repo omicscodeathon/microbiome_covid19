d1d0cec99d417b25657d2bf168c317600e16d611
Olaitan I. Awe <laitanawe@gmail.com>
2022-03-29 17:13:54 +0100

    Add new scripts

--- scripts/01_accessions_16s.sh
+++ scripts/01_accessions_16s.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 # This script grabs the first 105 accession ids from the Metadata file arranged by country and writes it to a new .txt file.
+# Countries in the Metadata: usa_wc, italy_milan, france, germany, india, northamerica_ny, japan_tokyo, usa_nashville, china 
+
+
 
 input_file="../accessions/microbiome_data.csv"
 echo "Input File: $input_file"
 
-output_file="../accessions/accessions_usa_16s.txt"
+output_file="../accessions/accessions_usa_wc_16s.txt"
 echo "Input File: $output_file"
 
 num_accessions=105
